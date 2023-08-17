abe8280936786ed4417b6fab0bb5da837ab45c5a
cristhiam.valle-ext@dominion-global.com <cristhiam.valle-ext@dominion-global.com>
2023-08-17 14:30:59 -0600

    Fix label table transfers

--- src/constant/inventory/inventoryTransfer/constantInventoryTransfer.js
+++ src/constant/inventory/inventoryTransfer/constantInventoryTransfer.js
@@ -1,7 +1,7 @@
 export const headersInventoryTransferTable = [
   { label: "Id", field: "transferId" },
-  { label: "Almacen de origen", field: "warehouseInputOrigin.name" },
-  { label: "Almacen destino", field: "warehouseInputDestination.name" },
+  { label: "Bodega de origen", field: "warehouseInputOrigin.name" },
+  { label: "Bodega destino", field: "warehouseInputDestination.name" },
   { label: "Producto", field: "product.name" },
   { label: "Cantidad", field: "quantity" },
   { label: "Acciones", field: "actions" },
